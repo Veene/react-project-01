1f6515b046f97a766e69323466b1076f6613539b
John Pawlak <johnpawlak@gmail.com>
2018-09-22 16:37:42 -0400

    first project setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hi Im a React App!!</h1>
+       <Person />
+      </div>
+      
+    )
+  }
+}
+
+export default App;
