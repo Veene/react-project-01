40b32013dfebaa3b13bafd7d7b230575d484dd41
John Pawlak <johnpawlak@gmail.com>
2018-09-24 22:13:08 -0400

    updated state tracking

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import UserOutput from './UserOutput';
 class App extends Component {
   state = {
     persons: [
-      {name: 'Max', age: 28 },
-      {name: "John", age: 29},
-      {name: "Monika", age: 29},
+      {id: 'asdas', name: 'Max', age: 28 },
+      {id: 'asdas1', name: "John", age: 29},
+      {id: 'asdas2', name: "Monika", age: 29},
     ],
-    username:'JohnnyBravo'
+    username:'JohnnyBravo',
+    showPersons: false,
   }
+
   usernameHandler = (newUsername) => {
     this.setState({
       username: newUsername.target.value
@@ -23,21 +25,43 @@ class App extends Component {
     // this.state.persons[0].name = 'Maximilian' DO NOT DO THIS
     this.setState({
       persons: [
-        {name: newName, age: 28 },
+        {name: newName.target.value, age: 28 },
         {name: "John", age: 29},
         {name: "Monika", age: 26},
       ]
     })
   }
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    // if true, it gives you the index
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    // grab that specific person object from inside persons array in state
+    const person = {
+      ...this.state.persons[personIndex]
+    } 
+    //updating the person who the ID corresponded to, so state change everything event.target.value changes
+    person.name = event.target.value;
+    // create copy of persons array from state, so that we are not mutating
+    const persons = [...this.state.persons]
+    //updating inside this array, the person object (got with Index) and then changing it to the person being updated via typing in input field
+    persons[personIndex] = person
+    //update the state finally with the new array of objects, in which the specified index person is changed
     this.setState({
-      persons: [
-        {name: 'Max', age: 28 },
-        {name: event.target.value, age: 29},
-        {name: "Monika", age: 26},
-      ]
+      persons:persons
     })
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
+  }
+  
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
   }
 
 
@@ -48,17 +72,53 @@ class App extends Component {
       border: '1px solid black',
       padding: '8px',
     };
+
+    let persons = null;
+
+    if (this.state.showPersons) { //can set if's and shit because not in the return statement yet
+      persons = (
+        <div>
+          {/* if we need an array, always use map, to work through the array and return what you need */}
+          {this.state.persons.map((person, index) => {
+            return <Person 
+            // to give index to the function need to use arrow function or else requires BIND
+            click={() => this.deletePersonHandler(index)}
+            key={person.id} 
+            name={person.name} 
+            age={person.age}
+            // once again, to pass it values (event, person.id) requires the arrow function
+            changed={(event) => this.nameChangedHandler(event, person.id)} />
+          })}
+          {/* <Person 
+          click={this.switchNameHandler} 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+          <Person 
+          changed={this.nameChangedHandler} 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}/>
+          <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/> */}
+        </div> 
+      )
+    }
+
     return (
       <div className="App">
         <h1>Hi Im a React App!!</h1>
-        <button style={style} onClick={() => this.switchNameHandler('MAXI!!')}>Switch Name</button>
-        <Person click={this.switchNameHandler} name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person changed={this.nameChangedHandler} name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-        <UserInput userChanged={this.usernameHandler} currentName={this.state.username}/>
+        <button style={style} 
+        onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        
+        {persons}
+        
+        
+        
+        
+        {/* <UserInput userChanged={this.usernameHandler} currentName={this.state.username}/>
         <UserOutput username={this.state.username}/>
         <UserOutput username={this.state.username}/>
-        <UserOutput username='John'/>
+        <UserOutput username='John'/> */}
         {/* <Person name="John" age="29">HOBBIES:HI IM JOHN</Person>
         <Person name="Ben" age="30"/> */}
       </div>
